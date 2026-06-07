cb510e689e957765ca8d013b27454d509dbbfed3
Cesar Guzman <cesar-gerardo@guzman-lopez.com>
2026-06-07 10:28:05 -0600

    fix: luarocks paths and treesitter runtimepath
    - Update luarocks paths from ~/.local to ~/.luarocks (pacman install dir)
    - Add treesitter site directory to runtimepath to fix healthcheck error

--- lua/options.lua
+++ lua/options.lua
@@ -2,13 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
+-- treesitter parsers install directory
+vim.opt.rtp:append(vim.fn.stdpath "data" .. "/site")
+
 -- luarocks paths for image.nvim (magick)
-local luarocks_path = vim.fn.expand "~/.local/share/lua/5.1/?.lua"
+local luarocks_path = vim.fn.expand "~/.luarocks/share/lua/5.1/?.lua"
   .. ";"
-  .. vim.fn.expand "~/.local/share/lua/5.1/?/init.lua"
+  .. vim.fn.expand "~/.luarocks/share/lua/5.1/?/init.lua"
 package.path = luarocks_path .. ";" .. package.path
 
-local luarocks_lib = vim.fn.expand "~/.local/lib/lua/5.1/?.so"
+local luarocks_lib = vim.fn.expand "~/.luarocks/lib/lua/5.1/?.so"
 package.cpath = luarocks_lib .. ";" .. package.cpath
 
 -- local o = vim.o
